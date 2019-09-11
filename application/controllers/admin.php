ffb7f7c9330c457089d66d396ca9afcb393351ad
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-09-11 22:38:53 +0700

    remake

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -6,7 +6,7 @@ class Admin extends CI_Controller
 	{
 		parent::__construct();
 
-		if ($this->session->userdata('status') != "login"){
+		if ($this->userdata('status') != "login"){
 			redirect(base_url("login"));
 		}
 	}
